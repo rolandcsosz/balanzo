2f3bce1dadf45698ab52e504c40a417fc5d4b206
Roland Csosz <hello@rolandcsosz.com>
2024-11-26 20:33:51 +0100

    Update desktop transaction view

--- ui/src/components/TransactionRow.tsx
+++ ui/src/components/TransactionRow.tsx
@@ -1,25 +1,30 @@
 
-import './Table.scss';
+import './TransactionRow.scss';
 import openButtonUrl from '../assets/open-button.svg';
+import { useBottomSheet } from '../hooks/useBottomSheet';
+import { formatDate } from '../utils/utlis';
+
 
 export const TransactionRow = ({ transaction }) => {
+    const { isOpen, content, openSheet, closeSheet } = useBottomSheet();
+
     return (
-        <div class="row">
-            <div class="cell itemCell">{transaction.item}</div>
-            <div class="cell amountCell">{transaction.amount}</div>
-            <div class="cell">
-                <span class="categoryPill">{transaction.subcategory.mainCategory.name}</span>
+        <div class="transaction-row">
+            <div class="transaction-cell transaction-item-cell">{transaction.item}</div>
+            <div class="transaction-cell transaction-amount-cell">{transaction.amount}</div>
+            <div class="transaction-cell">
+                <span class="transaction-category-pill">{transaction.subcategory.mainCategory.name}</span>
             </div>
-            <div class="cell">
-                <span class="subcategoryPill">{transaction.subcategory.name}</span>
+            <div class="transaction-cell">
+                <span class="transaction-subcategory-pill">{transaction.subcategory.name}</span>
             </div>
-            <div class="cell dateCell">{transaction.date}</div>
-            <div class=" cell actionCell">
-                <button class="actionButton">
+            <div class="transaction-cell transaction-date-cell">{formatDate(transaction.date)}</div>
+            <div class="transaction-action-cell">
+                <button class="transaction-action-button" onClick={() => { openSheet(<div>Heyy</div>) }}>
                     <img
                         src={openButtonUrl}
                         alt="Action"
-                        class="actionIcon"
+                        class="transaction-action-icon"
                         loading="lazy"
                     />
                 </button>
--- ui/src/pages/Transactions.tsx
+++ ui/src/pages/Transactions.tsx
@@ -1,25 +1,25 @@
-import './Transactions.scss';
+
 import { MetricCard } from '../components/MetricCard';
 import { Chart, ChartSize } from '../components/Chart';
 import { useTransactions } from '../hooks/useTransactions';
 import { Transaction } from '../types';
 import { useDevice } from '../hooks/useDevice';
 import { TransactionRow } from '../components/TransactionRow';
-import '../components/Table.scss';
+import './Transactions.scss';
 
 export function Transactions() {
     const transactions = useTransactions();
     const isMobile = useDevice();
 
     return (
-        <div class="table">
-            <header class="header">
-                <div class="headerCell">Item</div>
-                <div class="headerCell">Amount</div>
-                <div class="headerCell">Main category</div>
-                <div class="headerCell">Subcategory</div>
-                <div class="headerCell">Date</div>
-                <div class="headerCell"></div>
+        <div class="transaction-table">
+            <header class="transaction-header">
+                <div class="transaction-header-cell">Item</div>
+                <div class="transaction-header-cell">Amount</div>
+                <div class="transaction-header-cell">Main category</div>
+                <div class="transaction-header-cell">Subcategory</div>
+                <div class="transaction-header-cell">Date</div>
+                <div class="transaction-action-cell"></div>
             </header>
             {transactions.map(transaction => (
                 <TransactionRow
